92c8413547716d289fa621d4ec96ebb55c7ffafe
D'Orus Tsitera <dorustsitera@gmail.com>
2026-07-02 01:37:56 +0000

    feat:(metadata/init/exit): configure lkm template for simple helloworld
    Configure Makefile for helloworld module.
    Update template module metadata accordingly to helloworld module.
    Rename source file : lkm_template.c -> lkm_hello_world.c
    
    Signed-off-by: D'Orus Tsitera <dorustsitera@gmail.com>

--- lkm_hello_world.c
+++ lkm_hello_world.c
@@ -6,10 +6,11 @@
  * Intended as a starting point for out-of-tree kernel module development.
  *
  * Author:  D'Orus Tsitera
- * Date:    2026-06
+ * Date:    2026-07-02
  * Version: 0.1
  *
- * Tested on: Linux 6.1.175 x86_64
+ * Tested on: Linux dorus 6.18.37 #1 SMP PREEMPT_DYNAMIC x86_64 GNU/Linux
+
  */
 
 #define pr_fmt(fmt) "%s:%s(): " fmt, KBUILD_MODNAME, __func__
@@ -17,21 +18,22 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-MODULE_AUTHOR("Kaiwan N Billimoria");
-MODULE_DESCRIPTION("a simple LKM template; do refer to the (better) Makefile as well");
-MODULE_LICENSE("Dual MIT/GPL");	// or whatever
-MODULE_VERSION("0.2");
+MODULE_AUTHOR("D'Orus Tsitera");
+MODULE_DESCRIPTION("a simple hello world lkm");
+MODULE_LICENSE("GPL");	// or whatever
+MODULE_VERSION("0.1");
 
 static int __init lkm_template_init(void)
 {
-	pr_info("inserted\n");
+	pr_info("Hello lonely world !\n");
 	return 0;		/* success */
 }
 
 static void __exit lkm_template_exit(void)
 {
-	pr_info("removed\n");
+	pr_info("Goodebey lonely world !\n");
 }
 
 module_init(lkm_template_init);
 module_exit(lkm_template_exit);
+
